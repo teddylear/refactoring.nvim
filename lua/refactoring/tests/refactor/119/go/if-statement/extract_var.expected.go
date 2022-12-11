1a81a917ce07994388f4ed9552feeda9e8c2f0b5
teddylear <teddylear@gmail.com>
2022-12-11 16:17:10 -0500

    more updates

--- lua/refactoring/tests/refactor/119/go/if-statement/extract_var.expected.go
+++ lua/refactoring/tests/refactor/119/go/if-statement/extract_var.expected.go
@@ -13,8 +13,13 @@ type Order struct {
 func orderCalculation(order Order, a int, b int) (total float64) {
 	temp := a * b
 	basePrice := order.quantity*order.itemPrice
+    test := "poggers"
 blah := (basePrice) - 7
 	fmt.Println(blah, temp)
+foobar := "poggers"
+    if test == foobar {
+        fmt.Println("Sounds pretty poggers")
+    }
 	return basePrice -
 		math.Max(0, order.quantity-500)*order.itemPrice*0.05 +
 		math.Min(basePrice*0.1, 100)
--- lua/refactoring/tests/refactor/119/go/if-statement/extract_var.start.go
+++ lua/refactoring/tests/refactor/119/go/if-statement/extract_var.start.go
@@ -14,7 +14,11 @@ func orderCalculation(order Order, a int, b int) (total float64) {
 	temp := a * b
 	blah := (order.quantity *
 		order.itemPrice) - 7
+    test := "poggers"
 	fmt.Println(blah, temp)
+    if test == "poggers" {
+        fmt.Println("Sounds pretty poggers")
+    }
 	return order.quantity*order.itemPrice -
 		math.Max(0, order.quantity-500)*order.itemPrice*0.05 +
 		math.Min(order.quantity*

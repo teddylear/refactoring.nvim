a40d8b75f5abb730da97894ec8f6f7a21dea9471
teddylear <teddylear@gmail.com>
2022-12-19 22:23:03 -0500

    got new test working

--- lua/refactoring/tests/refactor/119/go/if-statement/extract_var.expected.go
+++ lua/refactoring/tests/refactor/119/go/if-statement/extract_var.expected.go
@@ -12,12 +12,12 @@ type Order struct {
 
 func orderCalculation(order Order, a int, b int) (total float64) {
 	temp := a * b
-      foobar := "poggers"
-       test := foobar
+	foobar := "poggers"
+	test := foobar
 	fmt.Println(temp)
-    if test == foobar {
-        fmt.Println("Sounds pretty poggers")
-    }
+	if test == foobar {
+		fmt.Println("Sounds pretty poggers")
+	}
 	return order.quantity*order.itemPrice -
 		math.Max(0, order.quantity-500)*order.itemPrice*0.05 +
 		math.Min(order.quantity*
--- lua/refactoring/tests/refactor/119/go/if-statement/extract_var.start.go
+++ lua/refactoring/tests/refactor/119/go/if-statement/extract_var.start.go
@@ -12,11 +12,11 @@ type Order struct {
 
 func orderCalculation(order Order, a int, b int) (total float64) {
 	temp := a * b
-    test := "poggers"
+	test := "poggers"
 	fmt.Println(temp)
-    if test == "poggers" {
-        fmt.Println("Sounds pretty poggers")
-    }
+	if test == "poggers" {
+		fmt.Println("Sounds pretty poggers")
+	}
 	return order.quantity*order.itemPrice -
 		math.Max(0, order.quantity-500)*order.itemPrice*0.05 +
 		math.Min(order.quantity*
